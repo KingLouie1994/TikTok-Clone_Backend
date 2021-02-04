4fcd74cad1c4844d4e6c01f975795c472bb8c26e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-04 19:25:52 +0100

    connected to DB using mongoose

---
+++ data.js
@@ -0,0 +1,22 @@
+const Data = [
+  {
+    url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    channel: "Example Channel",
+    description: "Here comes the description",
+    sound: "Example Sound - Drake",
+    likes: 143,
+    messages: 26,
+    shares: 8,
+  },
+  {
+    url: "https://www.w3schools.com/html/movie.mp4",
+    channel: "Second Example",
+    description: "What a creative description!",
+    sound: "Wrecking Ball - Hannah Montana",
+    likes: 98,
+    messages: 14,
+    shares: 11,
+  },
+];
+
+export default Data;
